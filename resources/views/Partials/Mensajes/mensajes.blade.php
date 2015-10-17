fda520ef9571c594cd13bcbc9144b53de6d97048
bucalexis <bucalexis@hotmail.com>
2015-10-17 14:04:22 -0500

    Se agrego el campo de error en el partial de mensajes, para agregar errores de forma manual

--- resources/views/Partials/Mensajes/mensajes.blade.php
+++ resources/views/Partials/Mensajes/mensajes.blade.php
@@ -13,3 +13,9 @@
     <p class="alert alert-success">{{ Session::get('message')  }}</p>
 
 @endif
+
+@if(Session::has('error'))
+
+    <ul class="alert alert-danger">{{ Session::get('error')  }}</ul>
+
+@endif
