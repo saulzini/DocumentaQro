3dfd7ee8bb643965c37c4aabd4275c8e1e6e1338
saulzini <saulzini@gmail.com>
2015-10-22 14:21:17 -0500

    Seeder FuncionesPeliculas
    Seeder FuncionesPeliculas

--- database/seeds/funcion_peliculaSeeder.php
+++ database/seeds/funcion_peliculaSeeder.php
@@ -12,6 +12,20 @@ class funcion_peliculaSeeder extends Seeder
     public function run()
     {
         //
-        factory(App\Funcion_Pelicula::class,10)->create();
+        $faker = Faker\Factory::create();
+        $funciones= DB::table('funciones')->lists('id');
+        $peliculas= DB::table('peliculas')->lists('id');
+
+        $i=0;
+        foreach ($funciones as $funcion)
+        {
+            $pelicula=$peliculas[$i];
+            $i++;
+            $funcioPeli=new \App\Funcion_Pelicula();
+            $funcioPeli->id_funcion=$funcion;
+            $funcioPeli->id_pelicula=$pelicula;
+            $funcioPeli->save();
+        }
+       // factory(App\Funcion_Pelicula::class,20)->create();
     }
 }
