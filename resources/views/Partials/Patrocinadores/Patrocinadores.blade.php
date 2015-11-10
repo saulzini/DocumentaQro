602eff33d48e60d4ae35d6a99ad64105e6926567
saulzini <saulzini@gmail.com>
2015-11-09 21:08:55 -0600

    Tabla responsiva y tooltips borrados
    Tabla responsiva y tooltips borrados

--- resources/views/Partials/Patrocinadores/Patrocinadores.blade.php
+++ resources/views/Partials/Patrocinadores/Patrocinadores.blade.php
@@ -137,9 +137,9 @@
         <div class="form-group" align="center">
             @if( isset($patrocinadoresItem))
 
-                {!! Form::submit(trans('validation.attributes.modificar'),['class'=>'btn btn-success btn-xs tooltips','data-placement'=>'top','data-original-title'=>trans('validation.attributes.modificar'), 'style'=>'width:20%','onclick'=>trans('validation.attributes.mensajeModificarPatrocinador')])!!}
+                {!! Form::submit(trans('validation.attributes.modificar'),['class'=>'btn btn-success btn-xs tooltips','data-placement'=>'top', 'style'=>'width:20%','onclick'=>trans('validation.attributes.mensajeModificarPatrocinador')])!!}
             @else
-                {!! Form::submit(trans('validation.attributes.agregar'),['class'=>'btn btn-success btn-xs tooltips','data-placement'=>'top','data-original-title'=>trans('validation.attributes.agregar'), 'style'=>'width:20%'])!!}
+                {!! Form::submit(trans('validation.attributes.agregar'),['class'=>'btn btn-success btn-xs tooltips','data-placement'=>'top', 'style'=>'width:20%'])!!}
             @endif
         </div>
 
--- resources/views/Partials/Realizadores/Realizadores.blade.php
+++ resources/views/Partials/Realizadores/Realizadores.blade.php
@@ -68,9 +68,9 @@
         <div class="form-group" align="center">
             @if( isset($realizadoresItem))
 
-                {!! Form::submit(trans('validation.attributes.modificar'),['class'=>'btn btn-success btn-xs tooltips','data-placement'=>'top','data-original-title'=>trans('validation.attributes.modificar'), 'style'=>'width:20%','onclick'=>trans('validation.attributes.mensajeModificarRealizador')])!!}
+                {!! Form::submit(trans('validation.attributes.modificar'),['class'=>'btn btn-success btn-xs tooltips','data-placement'=>'top', 'style'=>'width:20%','onclick'=>trans('validation.attributes.mensajeModificarRealizador')])!!}
             @else
-                {!! Form::submit(trans('validation.attributes.agregar'),['class'=>'btn btn-success btn-xs tooltips','data-placement'=>'top','data-original-title'=>trans('validation.attributes.agregar'), 'style'=>'width:20%'])!!}
+                {!! Form::submit(trans('validation.attributes.agregar'),['class'=>'btn btn-success btn-xs tooltips','data-placement'=>'top', 'style'=>'width:20%'])!!}
             @endif
         </div>
 
