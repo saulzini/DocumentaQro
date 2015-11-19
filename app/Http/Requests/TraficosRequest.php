76fb6ce2db639dc412f122d489595b65f9009fcc
bucalexis <bucalexis@hotmail.com>
2015-11-19 16:37:22 -0600

    traficos sin required realizador, integrante en server

--- app/Http/Requests/TraficosRequest.php
+++ app/Http/Requests/TraficosRequest.php
@@ -27,13 +27,10 @@ class TraficosRequest extends Request
         return [
             //
             'Pelicula'=>'required',
-            'Status'=>'required|in:Buscando contacto,Por enviar correo,Mail enviado,Mail respondido,
-            Ya con permiso,Ya con material,Por pagar,Pagado,En revision,Revisado',
+            'Status'=>'required|in:Buscando contacto,Por enviar correo,Mail enviado,Mail respondido,Ya con permiso,Ya con material,Por pagar,Pagado,En revision,Revisado',
             'Formato' =>'required',
             'Costo' => 'required',
             'Tipo' => 'required|in:Entrante,Saliente',
-            'Integrante' => 'required',
-            'Realizador' => 'required'
         ];
     }
 }
